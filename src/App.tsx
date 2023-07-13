7aeae3c11768d63b8854514b80bea10a0e9abdf9
davdev14 <davdev1400@gmail.com>
2023-07-13 18:35:00 +0100

    context setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AppContainer } from "./styles";
 import { Column } from "./Column";
 import { AddNewItem } from "./AddNewItem";
 
+
 export function App() {
   return (
     <AppContainer>
--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,20 +1,29 @@
-import React, { HtmlHTMLAttributes, useState } from "react";
+import React, { useState } from "react";
+import { useFocus } from "./utils/useFocus";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
 
 type NewItemFormProps = {
   onAdd(text: string): void;
 };
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
-  const [text, setText] = useState("")
+  const [text, setText] = useState("");
+  const inputRef = useFocus();
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter"){
+      onAdd(text)
+    }
+  }
   return (
-  <NewItemFormContainer>
-  <NewItemInput
-  value={text}
-  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
-  />
-  <NewItemButton onClick={() => onAdd(text)}>
-  Create
-  </NewItemButton>
-  </NewItemFormContainer>
-  )
-}
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setText(e.target.value)
+        }
+        onKeyDown={handleAddText}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
+};
